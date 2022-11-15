c39467eafe571c328fa650b8e74cfba06ae75a08
Dan Tamas <hi@dan-tamas.me>
2022-11-15 09:10:04 +0100

    add stacks and prepare error case

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Box } from "@mobily/stacks";
+
+import { dependencies } from "./package.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Box margin={10} padding={10} style={{ backgroundColor: "pink" }}>
+        <Text>{`@mobily/stacks: ${dependencies["@mobily/stacks"]}`}</Text>
+      </Box>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
